68e944024b08037eefb5dedbd50c5ed13e56fd67
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-10-04 19:56:44 +0300

    Old hello-world code was removed

---
+++ main/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: Oct 4, 2020
+ *      Author: vtitov
+ */
+
+#ifndef MAIN_MAIN_H_
+#define MAIN_MAIN_H_
+//-----------------------------------------------------------------------------
+// Define TAGs for log messages
+#define	TAG_MAIN	"APP"
+//-----------------------------------------------------------------------------
+#endif /* MAIN_MAIN_H_ */
